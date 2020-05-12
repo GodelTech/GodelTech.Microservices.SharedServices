e76a95b344a335ae9e9cb017a1f528996e9c8a70
diamond_dragon <diamond_dragon@tut.by>
2020-05-12 22:16:07 +0300

    Initial commit

---
+++ src/GodelTech.Microservices.SharedServices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GodelTech.Microservices.SharedServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
